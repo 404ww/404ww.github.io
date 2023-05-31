a27ca071b8689fec7e49d68bea6319bf88ceaef6
404ww <jingweiw83@gmail.com>
2023-05-31 22:28:04 +0800

    1

---
+++ a/js/index.js
@@ -0,0 +1,44 @@
+/** 欢迎使用guiPlan,以下代码均由guiPlan软件自动生成，如有疑问添加添加作者微信guiplan或加入学习交流qq群711964722  网站htpp://www.guiplan.com **/
+// interfaceCode
+var App = {
+    name: '',
+    mounted() {
+
+        // defaultLoad
+        // SheYingShouYeMountedStart
+        var wow = new WOW({
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 0,
+            mobile: true,
+            live: true
+        })
+        wow.init()
+        this.wow = wow
+        // SheYingShouYeMountedEnd
+
+    },
+    components: {},
+    data() {
+        return {
+            // insertData
+            // SheYingShouYeDataStart
+
+            // SheYingShouYeDataEnd
+
+        }
+    },
+    methods: {
+        default () {},
+        // insertMethod
+        // SheYingShouYeMethodStart
+
+        // SheYingShouYeMethodEnd
+
+        // methodsCode
+
+    },
+    computed: {}
+}
+const app = Vue.createApp(App);
+app.mount("#defaultId1");
